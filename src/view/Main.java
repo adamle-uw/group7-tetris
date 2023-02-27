b5b3463a34765cf0513e2029dbaf777008c26d1d
evan27uw <evan27@uw.edu>
2023-02-26 16:25:26 -0800

    Merged TempGUI into Main.java, making it so both groups' code works together in a main class.
    Deleted TempGUI

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,8 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
 
 public class Main extends JPanel {
     /**
@@ -23,6 +25,7 @@ public class Main extends JPanel {
         final int userWidth =  (int) size.getWidth();
         final int userHeight = (int) size.getHeight();
         frame.setSize(userWidth, userHeight);
+        frame.setJMenuBar(createFileMenu(frame));
         frame.setVisible(true);
 
         //panels
@@ -45,4 +48,82 @@ public class Main extends JPanel {
         nextPiece.setPreferredSize(new Dimension(userWidth / FOUR, userWidth / FOUR));
         userInfo.setPreferredSize(new Dimension(userWidth / FOUR, userHeight));
     }
+
+    public static JMenuBar createFileMenu(final JFrame theFrame) {
+        final JMenuBar fileMenu = new JMenuBar();
+        final JMenu gameMenu = new JMenu("Game");
+        final JMenu aboutMenu = new JMenu("About");
+        final JMenu optionsMenu = new JMenu("Options");
+        final JMenu newGameMenu = new JMenu("New Game");
+        JMenuItem menuItem;
+        menuItem = new JMenuItem("This Window");
+        //Sets a hotkey of CTRL + N to activate the event
+        menuItem.setAccelerator(
+                KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+        //Activates the event upon clicking the button or pressing the hotkey
+        menuItem.addActionListener(
+                theEvent -> System.out.println("Starting a new game in this window!"));
+        newGameMenu.add(menuItem);
+        menuItem = new JMenuItem("New Window");
+        menuItem.setAccelerator(
+                KeyStroke.getKeyStroke(KeyEvent.VK_N,
+                        InputEvent.CTRL_DOWN_MASK + InputEvent.SHIFT_DOWN_MASK));
+        //Activates the event upon clicking the button or pressing the hotkey
+        menuItem.addActionListener(
+                theEvent -> System.out.println("Starting a new game in a new window!"));
+        newGameMenu.add(menuItem);
+        gameMenu.add(newGameMenu);
+        //Adds item to the menu
+        //gameMenu.add(menuItem);
+        menuItem = new JMenuItem("Pause Game");
+        menuItem.addActionListener(theEvent -> System.out.println("Pausing game!"));
+        gameMenu.add(menuItem);
+        menuItem = new JMenuItem("Exit Game");
+        menuItem.setAccelerator(
+                KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
+        menuItem.addActionListener(theEvent -> theFrame.dispatchEvent(
+                new WindowEvent(theFrame, WindowEvent.WINDOW_CLOSING)));
+        gameMenu.add(menuItem);
+        menuItem = new JMenuItem("Controls");
+        menuItem.addActionListener(theEvent -> System.out.println("List controls here"));
+        menuItem.setAccelerator(
+                KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+        menuItem.addActionListener(
+                theEvent -> loadControlsGui());
+        aboutMenu.add(menuItem);
+        menuItem = new JMenuItem("Help");
+        menuItem.setAccelerator(
+                KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_DOWN_MASK));
+        menuItem.addActionListener(
+                theEvent -> System.out.println("Information about the game!"));
+        aboutMenu.add(menuItem);
+        menuItem = new JMenuItem("Option 1");
+        menuItem.addActionListener(
+                theEvent -> System.out.println("Option 1 working as intended"));
+        optionsMenu.add(menuItem);
+
+        //Adds the menus to the menu bar
+        fileMenu.add(gameMenu);
+        fileMenu.add(aboutMenu);
+        fileMenu.add(optionsMenu);
+
+        return fileMenu;
+    }
+
+    private static void loadControlsGui() {
+        SwingUtilities.invokeLater(() -> {
+            final Main controlsPanel = new Main();
+            final JFrame frame = new JFrame("Controls Menu");
+            final Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+            final int userWidth =  (int) size.getWidth();
+            final int userHeight = (int) size.getHeight();
+            frame.setSize(userWidth, userHeight);
+            frame.add(controlsPanel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
+
+
 }
