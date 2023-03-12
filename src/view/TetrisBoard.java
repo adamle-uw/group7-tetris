531e8a3b01cf44ac6ec18d2b43b8cbf95f0ad77e
evan27uw <evan27@uw.edu>
2023-03-12 16:19:37 -0700

    Fixed GUI for initial resolution
    Still working on changing resolutions, bug with clearing lines, and points

--- src/view/TetrisBoard.java
+++ src/view/TetrisBoard.java
@@ -1,83 +1,45 @@
 package view;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import model.Board;
+import model.Point;
+
+import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import javax.swing.JPanel;
-import model.Board;
-import model.Point;
 
-/**
- * This program ...
- *
- * @author Evan Abrahamson
- * @author Aryan Damle
- * @author Martha Emerson
- * @author Keegan Sanders
- * @version Winter 2023
- */
 public class TetrisBoard implements PropertyChangeListener {
     /**
-     * A constant for dividing board measurements in half.
+     * Avoid checkstyle 'magic error' number.
      */
     private static final int TWO = 2;
     /**
-     * The number of columns on the Tetris game board.
+     * Avoid checkstyle 'magic error' number for amount of columns.
      */
     private static final int COLUMNS = 10;
     /**
-     * The number of rows on the Tetris game board.
+     * Avoid checkstyle 'magic error' number for amount of rows.
      */
     private static final int ROWS = 20;
     /**
-     * A constant for calculating measurements related to the board display.
-     */
-    private static final int FORTY_EIGHT = 48;
-    /**
-     * A constant for calculating measurements related to the board display.
-     */
-    private static final int FIFTY = 50;
-    /**
-     * A constant for calculating measurements related to the board display.
-     */
-    private static final int FIVE_HUNDRED_NINE = 509;
-    /**
-     * A constant for calculating measurements related to the board display.
-     */
-    private static final int NINE_HUNDRED_SIXTY_EIGHT = 968;
-    /**
-     * The property value for Tetris Board Change. //fix this description (unclear)
+     * Property value for Tetris Board Change.
      */
     private static final String TETRIS_BOARD_PROPERTY = "TetrisBoardChange";
-    /**
-     * The JPanel that displays the Tetris game board. //which does what?
-     */
+    /**make tetris board.*/
     private final JPanel myTetrisBoard = new JPanel();
-    /**
-     * The width of ...
-     */
-    private final int myWidth;
-    /**
-     * The height of ...
-     */
-    private final int myHeight;
-    /**
-     * The JPanel that displays the Tetris game board. //which does what?
-     */
+    /**panel width.*/
+    private int myWidth;
+    /**panel height.*/
+    private int myHeight;
+    private int myXOffset;
+    private int myYOffset;
+    private int myCellWidth;
+    private int myCellHeight;
+
+    /**make board*/
     private final TetrisBoardJPanel myTetrisBoardJPanel = new TetrisBoardJPanel();
 
-    /**
-     * ...
-     *
-     * @param theUserWidth ...
-     * @param theUserHeight ...
-     * @param theBoard ...
-     */
     public TetrisBoard(final int theUserWidth, final int theUserHeight, final Board theBoard) {
         this.myTetrisBoard.setBackground(Color.black);
         this.myTetrisBoard.addPropertyChangeListener(this);
@@ -85,69 +47,80 @@ public class TetrisBoard implements PropertyChangeListener {
         myTetrisBoard.add(myTetrisBoardJPanel);
         myTetrisBoardJPanel.setVisible(true);
         myTetrisBoard.setVisible(true);
-        myWidth = myTetrisBoardJPanel.getWidth();
-        myHeight = myTetrisBoardJPanel.getHeight();
+        myWidth = (int)myTetrisBoardJPanel.getCurrentSize().getWidth();
+        myHeight = (int)myTetrisBoardJPanel.getCurrentSize().getHeight();
     }
 
-    /**
-     * ...
-     *
-     * @return ...
-     */
+
     public JPanel getTetrisBoard() {
         return myTetrisBoard;
     }
 
-    /**
-     * ...
-     *
-     * @param theEvent A PropertyChangeEvent object describing the event source
-     *          and the property that has changed.
-     */
     public void propertyChange(final PropertyChangeEvent theEvent) {
         if (TETRIS_BOARD_PROPERTY.equals(theEvent.getPropertyName())) {
             myTetrisBoardJPanel.repaint();
         }
         if ("Piece Location".equals(theEvent.getPropertyName())) {
-            final int xOffset = (509 - (COLUMNS * (509 / COLUMNS))) / TWO;
-            final int yOffset = (968 - (ROWS * (968 / ROWS))) / TWO;
+            myXOffset = (myWidth - (COLUMNS * (myWidth / COLUMNS))) / TWO;
+            myYOffset = (myHeight - (ROWS * (myHeight / ROWS))) / TWO;
+            myCellWidth = myWidth / COLUMNS;
+            myCellHeight = myHeight / ROWS;
             myTetrisBoardJPanel.myMovingCells.clear();
-            final Point[] p = (Point[]) theEvent.getNewValue();
+            Point p[] = (Point[])theEvent.getNewValue();
 
             for (Point p2 : p) {
-                final int x = xOffset + (p2.x() * (509 / COLUMNS));
-                final int y = yOffset + (p2.y() * (968 / ROWS));
-                final Rectangle r = new Rectangle(x, y);
-                r.setLocation(x, -(y - (NINE_HUNDRED_SIXTY_EIGHT
-                        - (NINE_HUNDRED_SIXTY_EIGHT / ROWS))));
-                r.setSize(FIFTY, FORTY_EIGHT);
+                int x = myXOffset + (p2.x() * myCellWidth);
+                int y = myYOffset + (p2.y() * myCellHeight);
+                Rectangle r = new Rectangle(x,y);
+                r.setLocation(x, -(y - (myHeight - myCellHeight)));
+                r.setSize(myCellWidth, myCellHeight);
                 myTetrisBoardJPanel.myMovingCells.add(r);
             }
+            myWidth = (int)myTetrisBoardJPanel.getCurrentSize().getWidth();
+            myHeight = (int)myTetrisBoardJPanel.getCurrentSize().getHeight();
+            System.out.println("Width: " + myWidth + " Height: " + myHeight);
+
+            /*ArrayList<Rectangle> rList = new ArrayList(COLUMNS * ROWS);
+            for (Rectangle r : myTetrisBoardJPanel.myPlacedCells) {
+                System.out.println("x " + (int)r.getLocation().getX());
+                int x = ((int)r.getLocation().getX());
+                int y = ((int)r.getLocation().getY());
+                System.out.println("y " + y);
+                r.setLocation(x, (y));
+                r.setSize(myCellWidth, myCellHeight);
+                rList.add(r);
+                System.out.println(rList);
+            }
+            myTetrisBoardJPanel.myPlacedCells.clear();
+            for (Rectangle r : rList) {
+                myTetrisBoardJPanel.myPlacedCells.add(r);
+            }
+
+             */
+
             myTetrisBoardJPanel.repaint();
         }
         if ("PlacedBlock".equals(theEvent.getPropertyName())) {
-            final int xOffset = (509 - (COLUMNS * (509 / COLUMNS))) / TWO;
-            final int yOffset = (968 - (ROWS * (968 / ROWS))) / TWO;
-            final Point[] p = (Point[]) theEvent.getNewValue();
+            Point p[] = (Point[])theEvent.getNewValue();
 
             for (Point p2 : p) {
-                final int x = xOffset + (p2.x() * (509 / COLUMNS));
-                final int y = yOffset + (p2.y() * (968 / ROWS));
-                final Rectangle r = new Rectangle(x, y);
-                r.setLocation(x, -(y - (NINE_HUNDRED_SIXTY_EIGHT
-                        - (NINE_HUNDRED_SIXTY_EIGHT / ROWS))));
-                r.setSize(FIFTY, FORTY_EIGHT);
+                int x = myXOffset + (p2.x() * myCellWidth);
+                int y = myYOffset + (p2.y() * myCellHeight);
+                Rectangle r = new Rectangle(x,y);
+                r.setLocation(x, -(y - (myHeight - myCellHeight)));
+                r.setSize(myCellWidth, myCellHeight);
                 myTetrisBoardJPanel.myPlacedCells.add(r);
             }
             myTetrisBoardJPanel.repaint();
         }
         if ("Row Location".equals(theEvent.getPropertyName())) {
-            final ArrayList<Rectangle> rTemp = new ArrayList<>(COLUMNS * ROWS);
-            final int rowY = 916 - ((int) theEvent.getNewValue() * 48);
+            ArrayList<Rectangle> rTemp = new ArrayList(COLUMNS * ROWS);
+            final int rowY = myHeight - myYOffset - ((int)theEvent.getNewValue() * (myHeight / ROWS));
+            System.out.println(rowY);
             for (Rectangle r : myTetrisBoardJPanel.myPlacedCells) {
-                if ((int) r.getY() != rowY) {
-                    r.setLocation((int) r.getX(), (int) r.getY()
-                            + (FIVE_HUNDRED_NINE / COLUMNS));
+                if ((int)r.getY() != rowY) {
+                    System.out.println(rowY + " " + (int)r.getY());
+                    r.setLocation((int)r.getX(), (int)r.getY() + (myHeight / ROWS));
                     rTemp.add(r);
                 }
             }
@@ -161,81 +134,42 @@ public class TetrisBoard implements PropertyChangeListener {
         }
     }
 
-    /**
-     * ...
-     *
-     * @return ...
-     */
     public ArrayList<Rectangle> getCells() {
         return myTetrisBoardJPanel.getMyCells();
     }
 
-    /**
-     * ...
-     */
     private static class TetrisBoardJPanel extends JPanel {
-        /**
-         * ...
-         */
-        private final int myColumnCount = 10;
-        /**
-         * ...
-         */
-        private final int myRowCount = 20;
-        /**
-         * ...
-         */
+
+        private int myColumnCount = 10;
+        private int myRowCount = 20;
         private final ArrayList<Rectangle> myCells;
-        /**
-         * ...
-         */
         private final ArrayList<Rectangle> myMovingCells;
-        /**
-         * ...
-         */
         private final ArrayList<Rectangle> myPlacedCells;
-        /**
-         * ...
-         */
         TetrisBoardJPanel() {
-            myCells = new ArrayList<>(myColumnCount * myRowCount);
-            myMovingCells = new ArrayList<>(myColumnCount * myRowCount);
-            myPlacedCells = new ArrayList<>(myColumnCount * myRowCount);
+            myCells = new ArrayList<Rectangle>(myColumnCount * myRowCount);
+            myMovingCells = new ArrayList<Rectangle>(myColumnCount * myRowCount);
+            myPlacedCells = new ArrayList<Rectangle>(myColumnCount * myRowCount);
         }
 
-        /**
-         * ...
-         * @return ...
-         */
         public ArrayList<Rectangle> getMyCells() {
             return myCells;
         }
 
-        /**
-         * ...
-         *
-         * @return ...
-         */
         @Override
         public Dimension getPreferredSize() {
             return new Dimension(this.getParent().getHeight() / 2,
-                    this.getParent().getHeight() - FIFTY);
+                    this.getParent().getHeight() - 50);
         }
 
-        /**
-         * ...
-         */
+        public Dimension getCurrentSize() {
+            return new Dimension(getWidth(), getHeight());
+        }
+
         @Override
         public void invalidate() {
             myCells.clear();
             super.invalidate();
         }
-
-        /**
-         * ...
-         *
-         * @param theG the <code>Graphics</code> object to protect
-         */
         protected void paintComponent(final Graphics theG) {
             super.paintComponent(theG);
             final Graphics2D g2d = (Graphics2D) theG.create();
