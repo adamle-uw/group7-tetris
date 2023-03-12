e6b40bf2089cb0b08e6d0a1fe410c745b8c844e9
Keegan Sanders <46981601+sanderskn11@users.noreply.github.com>
2023-03-11 23:45:53 -0800

    We have a board!

--- src/view/TetrisBoard.java
+++ src/view/TetrisBoard.java
@@ -1,29 +1,34 @@
 package view;
 
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class TetrisBoard implements PropertyChangeListener {
     /**
      * Avoid checkstyle 'magic error' number.
      */
-    private static final int FOUR = 4;
+    private static final int TWO = 2;
     /**
      * Property value for Tetris Board Change.
      */
     private static final String TETRIS_BOARD_PROPERTY = "TetrisBoardChange";
     /**make tetris board.*/
-    private final JPanel myTetrisBoard;
+    private final JPanel myTetrisBoard = new JPanel();
+    /**make board*/
+    private final TetrisBoardJPanel myTetrisBoardJPanel = new TetrisBoardJPanel();
 
     public TetrisBoard(final int theUserWidth, final int theUserHeight) {
-        myTetrisBoard = new JPanel();
         this.myTetrisBoard.setBackground(Color.black);
         this.myTetrisBoard.addPropertyChangeListener(this);
-        myTetrisBoard.setPreferredSize(new Dimension(theUserWidth / FOUR, theUserHeight));
+        myTetrisBoard.setPreferredSize(new Dimension(theUserWidth, theUserHeight));
+        myTetrisBoard.add(myTetrisBoardJPanel);
+        myTetrisBoardJPanel.setVisible(true);
+        myTetrisBoard.setVisible(true);
     }
+
 
     public JPanel getTetrisBoard() {
         return myTetrisBoard;
@@ -31,7 +36,63 @@ public class TetrisBoard implements PropertyChangeListener {
 
     public void propertyChange(final PropertyChangeEvent theEvent) {
         if (TETRIS_BOARD_PROPERTY.equals(theEvent.getPropertyName())) {
+            myTetrisBoardJPanel.repaint();
+        }
+    }
+
+    private static class TetrisBoardJPanel extends JPanel {
+
+        private int myColumnCount = 10;
+        private int myRowCount = 20;
+        private ArrayList<Rectangle> myCells;
+        TetrisBoardJPanel() {
+            myCells = new ArrayList<Rectangle>(myColumnCount * myRowCount);
 
         }
+
+        public ArrayList<Rectangle> getMyCells() {
+            return myCells;
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(this.getParent().getHeight() / 2,
+                    this.getParent().getHeight() - 50);
+        }
+
+        @Override
+        public void invalidate() {
+            myCells.clear();
+            super.invalidate();
+        }
+        protected void paintComponent(final Graphics theG) {
+            super.paintComponent(theG);
+            final Graphics2D g2d = (Graphics2D) theG.create();
+
+            final int width = getWidth();
+            final int height = getHeight();
+
+            final int cellWidth = width / myColumnCount;
+            final int cellHeight = height / myRowCount;
+
+            final int xOffset = (width - (myColumnCount * cellWidth)) / 2;
+            final int yOffset = (height - (myRowCount * cellHeight)) / 2;
+
+            for (int row = 0; row < myRowCount; row++) {
+                for (int col = 0; col < myColumnCount; col++) {
+                    final Rectangle cell = new Rectangle(xOffset + (col * cellWidth),
+                            yOffset + (row * cellHeight), cellWidth, cellHeight);
+                    myCells.add(cell);
+                }
+            }
+
+            g2d.setColor(Color.GRAY);
+            for (Rectangle cell : myCells) {
+                g2d.draw(cell);
+            }
+
+            g2d.dispose();
+        }
+
     }
 }
